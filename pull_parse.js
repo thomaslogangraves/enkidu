8dcf4aeb046953880cf59935cf3953210256c1e2
Thomas Graves <logan@localhost.localdomain>
2016-11-07 19:01:07 -0500

    Working on response from running Script.

--- pull_parse.js
+++ pull_parse.js
@@ -1,11 +1,11 @@
 var parse = require('child_process').spawn,
   data = "HR.csv",
-  py    = parse('python', ['parser.py', data]),
+  py = parse('python', ['parser.py', data]),
   dataString = '';
 
 /*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), we want to convert that received data into a string and append it to the overall dataString.*/
 py.stdout.on('data', function(data){
-  dataString += data.toString();
+  dataString += data.toString()
 });
 
 /*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
@@ -15,3 +15,5 @@ py.stdout.on('end', function(){
 /*We have to stringify the data first otherwise our python process wont recognize it*/
 py.stdin.write(JSON.stringify(data));
 py.stdin.end();
+
+return dataString;
