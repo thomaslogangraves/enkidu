bada1507728fe0481360f8a5581fed890ce8431b
Thomas Graves <logan@localhost.localdomain>
2016-11-16 21:19:00 -0500

    converting to node only process"

--- application.js
+++ application.js
@@ -1,9 +1,9 @@
-var parser = require('./pull_parse')
-var compute = require('./compute');
+const parser = require('./pull_parse')
+const compute = require('./compute');
 
-var file = "HR.csv"
 
-var value = parser(file)
-var computation = compute([1,2,3,4,5,6]);
-console.log(value, "this is the value");
-console.log("computation", computation);
+const fileName = "HR.csv"
+console.log(parser.parseTool());
+// const computation = compute([1,2,3,4,5,6]);
+
+// console.log("computation", computation);
--- pull_parse.js
+++ pull_parse.js
@@ -1,23 +1,16 @@
- var parser = function(fileName){
-  var parse = require('child_process').spawn,
-    data = fileName,
-    py = parse('python', ['parser.py', data]),
-    dataString = '';
+module.exports.parseTool = function() {
+  const fs = require('fs');
+  const csv = require('fast-csv');
+  const dataSet = [];
 
-    // py.stdout.pipe(process.stdout);
-    // py.stderr.pipe(process.stderr);
-  /*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), we want to convert that received data into a string and append it to the overall dataString.*/
-  py.stdout.on('data', function(data){
-    dataString += data.toString();
-  });
+  fs.createReadStream('./HR.csv')
+    .pipe(csv())
+    .on('data', function(data){
+      dataSet.push(data);
+    })
+    .on('end', function(data){
+      dataSet.splice(0, 2)
+      return dataSet
 
-  /*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
- py.stdout.on('end', function(){
-    console.log(dataString);
-  });
-  py.stdin.write(JSON.stringify(data));
-  py.stdin.end();
-
+    });
 }
-
-module.exports = parser;
